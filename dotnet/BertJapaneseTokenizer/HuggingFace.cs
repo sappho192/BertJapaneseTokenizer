bf159b0750107c2079053dea733351b46cc4bfe6
TAE-IN Sappho KIM <sappho192@gmail.com>
2024-01-18 14:10:15 +0900

    Add functionality to download vocab.txt from hf

---
+++ dotnet/BertJapaneseTokenizer/HuggingFace.cs
@@ -0,0 +1,45 @@
+﻿namespace BertJapaneseTokenizer
+{
+    public static class HuggingFace
+    {
+        /// <summary>
+        /// <para>Download the vocab.txt file in the hub.</para>
+        /// <para>Example of supported hub: cl-tohoku/bert-base-japanese, cl-tohoku/bert-large-japanese-v2, etc.</para>
+        /// </summary>
+        /// <param name="hubName"></param>
+        /// <returns></returns>
+        public static async Task<string> GetVocabFromHub(string hubName)
+        {
+            // Example of hubName: cl-tohoku/bert-large-japanese
+            // We need to separate the hubName, delimiter is '/'
+            // And then concatenate the path using Path.Combine
+            string[] sepPath = hubName.Split('/');
+            string directory = Path.Combine(sepPath);
+            string vocabPath = Path.Combine(directory, "vocab.txt");
+            // Check if the vocab.txt file exists in the directory
+            // Return path if it exists
+            if (File.Exists(vocabPath))
+            {
+                return vocabPath;
+            }
+
+            // Download the vocab.txt file from the hub
+            // Example url: https://huggingface.co/cl-tohoku/bert-large-japanese/resolve/main/vocab.txt?download=true
+            var url = $"https://huggingface.co/{hubName}/resolve/main/vocab.txt?download=true";
+            // Using HttpClient, save into a path {hubName}/vocab.txt, and return the path
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            // Create the directory if it doesn't exist
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception($"Cannot download vocab.txt from {url}");
+            }
+            Directory.CreateDirectory(directory);
+            using (var fileStream = File.Create(vocabPath))
+            {
+                await response.Content.CopyToAsync(fileStream);
+            }
+            return vocabPath;
+        }
+    }
+}
--- dotnet/ConsoleExample/Program.cs
+++ dotnet/ConsoleExample/Program.cs
@@ -1,9 +1,12 @@
-﻿var dicPath = @"D:\DATASET\unidic-mecab-2.1.2_bin";
-var vocabPath = @"D:\DATASET\bert-japanese\bert-base-japanese-v2\vocab.txt";
+﻿using BertJapaneseTokenizer;
+
+var dicPath = @"D:\DATASET\unidic-mecab-2.1.2_bin";
+//var vocabPath = @"D:\DATASET\bert-japanese\bert-base-japanese-v2\vocab.txt";
+var vocabPath = await HuggingFace.GetVocabFromHub("cl-tohoku/bert-base-japanese-v2");
 var tokenizer = new BertJapaneseTokenizer.BertJapaneseTokenizer(dicPath, vocabPath);
 
-var sentence = "打ち合わせが終わった後にご飯を食べましょう.";
-//var sentence = "ご飯を食べましょう.";
+var sentence = "打ち合わせが終わった後にご飯を食べましょう。";
+//var sentence = "ご飯を食べましょう。";
 //var sentence = "打ち合わせ";
 
 int[] tokenIds = tokenizer.EncodePlus(sentence);
